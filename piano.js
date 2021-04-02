e2ed538fc89790538419eeaa0587d5f78cba7df1
Sneha Nangare <81547445+sneha101-bit@users.noreply.github.com>
2021-04-02 16:35:04 +0530

    Add files via upload

---
+++ piano.js
@@ -0,0 +1,76 @@
+
+document.addEventListener('keydown', function(event) {
+    if (event.code == "KeyA")
+    {
+        let audio = new Audio("A.mp3");
+        audio.play()
+    }
+    else if (event.code == "KeyS")
+    {
+        let audio = new Audio("S.mp3");
+        audio.play()
+    }
+    else if (event.code == "KeyD")
+    {
+        let audio = new Audio("D.mp3");
+        audio.play()
+    }
+    else if (event.code == "KeyF")
+    {
+        let audio = new Audio("F.mp3");
+        audio.play()
+    }
+    else if (event.code == "KeyG")
+    {
+        let audio = new Audio("G.mp3");
+        audio.play()
+    }
+    else if (event.code == "KeyH")
+    {
+        let audio = new Audio("H.mp3");
+        audio.play()
+    }
+    else if (event.code == "KeyW")
+    {
+        let audio = new Audio("W.mp3");
+        audio.play()
+    }
+    else if (event.code == "KeyE")
+    {
+        let audio = new Audio("E.mp3");
+        audio.play()
+    }
+    else if (event.code == "KeyT")
+    {
+        let audio = new Audio("T.mp3");
+        audio.play()
+    }
+    else if (event.code == "KeyY")
+    {
+        let audio = new Audio("Y.mp3");
+        audio.play()
+    }
+    else if (event.code == "KeyU")
+    {
+        let audio = new Audio("U.mp3");
+        audio.play()
+    }
+    else if (event.code == "KeyJ")
+    {
+        let audio = new Audio("J.mp3");
+        audio.play()
+    }
+})
+
+
+/*
+    document.addEventListener('keydown', function(event) {
+        if ((event.code == "KeyA") || (event.code == "KeyS") || (event.code == "KeyD") ||
+            (event.code == "KeyF") || (event.code == "KeyG") || (event.code == "KeyH") ||
+            (event.code == "KeyJ")) {
+            let audio =
+                console.log("The '" + event.key +"' key is pressed.")
+        } else {
+            console.log("Invalid key is pressed.")
+        }
+    })*/
